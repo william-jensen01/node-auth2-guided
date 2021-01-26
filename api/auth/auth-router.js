a86fb0db23bcc08c0cc9ea3244b6ba912c0ebab3
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-26 19:13:43 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -66,7 +66,7 @@ function generateToken(user) {
     expiresIn: '1d',
   }
 
-  return jwt.sign(payload, jwtSecret, options)
+  return jwt.sign(payload, null, options)
 }
 
 module.exports = router;
