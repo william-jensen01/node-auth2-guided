b4a3ba46dad858045e6ba8126e2a130d95687afa
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-26 19:26:20 +0100

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken')
 
+
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (token) {
-    //
+    jwt.verify()
   } else {
     res.status(401).json('we wantz token')
   }
