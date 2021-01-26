b20eaaf01c5fb89010522f920330bef4da1eed42
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-26 19:51:09 +0100

    wip

--- api/auth/rolerestricted-middleware.js
+++ api/auth/rolerestricted-middleware.js
@@ -2,6 +2,6 @@ module.exports = role => (req, res, next) => {
   if (req.decodedJwt.role === role) {
     next()
   } else {
-    res.status(403)
+    res.status(403).json('you have no power here')
   }
 }
