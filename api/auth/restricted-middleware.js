ad4e4fc6862b677b932906a8f825a271fecac85b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-01-26 20:00:16 +0100

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -3,7 +3,7 @@ const { jwtSecret } = require('../../config/secrets.js')
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
-  if (token) {
+  if (token && tokenNotInListOfBadTokens(token)) {
     jwt.verify(token, jwtSecret, (err, decoded) => {
       if (err) {
         res.status(401).json('we wantz VALID token')
